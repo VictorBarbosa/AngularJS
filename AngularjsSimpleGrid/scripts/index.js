0ed5ec6e8057278559eeef8e41ddded3cee389c2
victorbarbosa <victor.hugo.p.barbosa@hotmail.com>
2016-01-18 21:37:59 -0200

    First commit

---
+++ AngularjsSimpleGrid/scripts/index.js
@@ -0,0 +1,11 @@
+﻿
+angular.module("app", []);
+angular.module("app", []).controller("control", function ($scope) {
+    $scope.Datas = [
+  { "name": "Victor", "tel": "555-55555", "cel": '999999-9999', "email": "Victor@Victor.com" },
+  { "name": "Andre", "tel": "555-55555", "cel": '999999-9999', "email": "Andre@Andre.com" },
+  { "name": "Jonh", "tel": "555-55555", "cel": '999999-9999', "email": "Jonh@Jonh.com" },
+  { "name": "Bill", "tel": "555-55555", "cel": '999999-9999', "email": "Bill@Bill.com" },
+
+    ];
+});
